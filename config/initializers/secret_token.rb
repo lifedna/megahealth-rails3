3fc79849890503c95a8892898b09d82ed8f5f486
wangyang <wangyang@wangyang-Latitude-E6530.(none)>
2013-07-13 12:30:27 +0800

    rails_apps_composer: initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+MegahealthRails3::Application.config.secret_token = '0337de4243ff10fc4091405e10d61f1c7d46b7d0c9b2a5d074af9d513799757a0c68a803474e63b807d4e00a9c9f696cf81085962766b5204e529eab03ac3ea8'
