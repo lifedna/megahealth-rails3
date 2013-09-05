268dcb5968ef2a3e0173e019d98d22c65e3f406a
wangyang <wangyang@wangyang-Latitude-E6530.(none)>
2013-09-05 10:39:24 +0800

    init

---
+++ app/helpers/stories_helper.rb
@@ -0,0 +1,19 @@
+# coding: utf-8
+module StoriesHelper
+  def sort_by(string)
+  	case string
+  	when "newest"
+  	  return "最新"
+  	when "most_viewed"
+  	  return "浏览"	
+  	when "most_commented"
+  	  return "评论"	
+  	when "most_useful"
+  	  return "有用"	
+  	when "most_cheered"
+  	  return "鼓励"
+  	else
+  	  return "最新"	
+  	end
+  end	
+end
