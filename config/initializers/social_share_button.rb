db8333e2276f08130221977626eb93b8df4475fc
wangyang <wangyang@wangyang-Latitude-E6530.(none)>
2013-09-09 14:39:34 +0800

    init

--- config/initializers/social_share_button.rb
+++ config/initializers/social_share_button.rb
@@ -1,3 +1,4 @@
 SocialShareButton.configure do |config|
-  config.allow_sites = %w(twitter facebook google_plus weibo douban tqq renren qq kaixin001 baidu google_bookmark delicious huaban tumblr)
+  config.allow_sites = %w(weibo douban tqq renren qq baidu)	
+  # config.allow_sites = %w(twitter facebook google_plus weibo douban tqq renren qq kaixin001 baidu google_bookmark delicious huaban tumblr)
 end
