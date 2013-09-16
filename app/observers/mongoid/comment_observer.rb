6ead438aab6b01048a56835f6362b858ca43bb44
wangyang <wangyang@wangyang-Latitude-E6530.(none)>
2013-09-16 23:00:35 +0800

    init

--- app/observers/mongoid/comment_observer.rb
+++ app/observers/mongoid/comment_observer.rb
@@ -1,10 +1,12 @@
 class Mongoid::CommentObserver < Mongoid::Observer
   def after_create(record)
   	if record.commenter != record.commentabled_obj.user
-  	  notification = Notification.find_or_create_by(has_been_read:"unread", receiver:record.commentabled_obj.user, notifiable:record.commentable)
-  	  notification.verb = "new_comment"
-  	  notification.inc(:count, 1)
-  	  notification.save
+  	  if record.commentable_type.in?(['Story', 'Topic', 'Question'])	
+	  	notification = Notification.find_or_create_by(has_been_read:"unread", receiver:record.commentabled_obj.user, notifiable:record.commentable)
+	  	notification.verb = "new_comment"
+	  	notification.inc(:count, 1)
+	  	notification.save
+	  end
 	end
   end
 end
